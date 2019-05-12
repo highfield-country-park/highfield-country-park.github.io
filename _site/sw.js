67b7848ccf66d7baa65222960cbd5f78562a49f0
alex <alex.james.lee@gmail.com>
2019-05-12 12:24:14 +0100

    add contact

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20190511205545';
+const version = '20190512122348';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/event/2019/05/10/join-our-first-volunteering-saturday-session/","/event/2019/05/05/new-event-post/","/event/2019/03/03/mid-event-post/","/event/2019/01/01/event-post/","/news/2019/01/01/news-post/","/event/2018/08/22/hello-world/","/about/","/categories/","/events/","/blog/","/","/map/","/news/","/posts/","/volunteer/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/event/2019/05/12/changes-to-the-website/","/event/2019/05/10/join-our-first-volunteering-saturday-session/","/event/2019/05/05/new-event-post/","/event/2019/03/03/mid-event-post/","/event/2019/01/01/event-post/","/news/2019/01/01/news-post/","/event/2018/08/22/hello-world/","/about/","/categories/","/events/","/blog/","/","/map/","/news/","/posts/","/volunteer/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
